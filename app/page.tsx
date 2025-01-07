2d1bae1ee0a66bbb5b85fc5b5099e1d74d3905a2
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-01-07 01:27:42 +0100

    create NavBar component with new functionality and routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import NavBar from '@/components/NavBar'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      <NavBar />
     </div>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,36 +1,84 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import logo from "@/public/images/logo.png"
 import Link from 'next/link'
 import { TbLogin2 } from "react-icons/tb";
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation'
 export default function NavBar() {
+    const pathname = usePathname()
+    console.log(pathname)
+    const showLine = (e: MouseEvent, isHover: boolean = false) => {
+        e?.preventDefault()
+        const element = e.currentTarget.lastElementChild?.classList
+        if (isHover) {
+            element?.replace("opacity-0", "opacity-100")
+        } else {
+            element?.replace("opacity-100", "opacity-0")
+        }
+
+    }
     return (
         <div className='p-4 shadow-xl flex items-center w-full justify-between'>
-            <div className='flex items-center gap-x-4'>
+            <Link href={"/"} className='flex items-center gap-x-4'>
                 <Image src={logo.src} alt='' width={900} height={900} className=' h-[53px] w-[70px]' />
-                <h1 className='font-semibold text-xl text-[#1E1E6C]'>إقامة أبواب بوسكورة GH 3</h1>
-            </div>
-            <ul className='flex justify-center gap-x-32 font-semibold'>
-                <li className='hover:text-[#1E1E6C] flex'>
-                    <h5>
+                <h1 className='font-semibold text-xl text-[#1E1E6C]'>إقامات أبواب بوسكورة GH 3</h1>
+            </Link>
+            <ul className='flex justify-start gap-x-32 font-semibold'>
+                <li
+                    onMouseEnter={
+                        (event: MouseEvent) => showLine(event, true)
+                    }
+                    onMouseLeave={
+                        (event) => showLine(event, false)
+                    }
+                    className={cn('hover:text-[#1E1E6C] flex flex-col',
+                        pathname === '/' && 'text-[#1E1E6C]'
+                    )}>
+                    <Link href={"/"}>
                         الرئيسية
-                    </h5>
-                    <span className='w-full bg-[#1E1E6C] h-2'></span>
+                    </Link>
+                    <span className={cn('w-full bg-[#1E1E6C] h-2 rounded duration-700',
+                        pathname !== '/' ? 'opacity-0' : "bg-[#1E1E6C]"
+                    )}></span>
                 </li>
-                <li className='hover:text-[#1E1E6C] flex flex-col gap-y-1'>
-                    <h5>
+                <li
+                    onMouseEnter={
+                        (event: MouseEvent) => showLine(event, true)
+                    }
+                    onMouseLeave={
+                        (event) => showLine(event, false)
+                    }
+                    className={cn('hover:text-[#1E1E6C] flex flex-col',
+                        pathname === '/monthly-list' && 'text-[#1E1E6C]'
+                    )}>
+                    <Link href={'/monthly-list'}>
                         لائحة الأداء الشهري
-                    </h5>
-                    <span className='w-full bg-[#1E1E6C] h-2 rounded-md'></span>
+                    </Link>
+                    <span className={cn('w-full bg-[#1E1E6C] h-2 rounded-md duration-700',
+                        pathname !== '/monthly-list' ? 'opacity-0' : "bg-[#1E1E6C]"
+                    )}></span>
                 </li>
-                <li className='hover:text-[#1E1E6C] flex'>
-                    <h5>
+                <li
+                    onMouseEnter={
+                        (event: MouseEvent) => showLine(event, true)
+                    }
+                    onMouseLeave={
+                        (event) => showLine(event, false)
+                    }
+                    className={cn('hover:text-[#1E1E6C] flex flex-col',
+                        pathname === '/announcement' && 'text-[#1E1E6C]'
+                    )}>
+                    <Link href={"/announcement"}>
                         الإعلان
-                    </h5>
-                    <span className='w-full bg-[#1E1E6C] h-2'></span>
+                    </Link>
+                    <span className={cn('w-full bg-[#1E1E6C] h-2 rounded-md duration-700',
+                        pathname !== '/announcement' ? 'opacity-0' : "bg-[#1E1E6C]"
+                    )}></span>
                 </li >
             </ul>
-            <Link href={""} className='rounded-md p-4 bg-[#E3F8FA] shadow-lg shadow-[#26C6DA] hover:text-white duration-300 hover:bg-[#76B9BF] hover:shadow-[#76B9BF] flex items-center gap-4 font-semibold' >تسجيل الدخول <TbLogin2 size={25} /></Link>
+            <Link href={"/login"} className='rounded-md p-4 bg-[#E3F8FA] shadow-lg shadow-[#26C6DA] hover:text-white duration-300 hover:bg-[#76B9BF] hover:shadow-[#76B9BF] flex items-center gap-4 font-semibold' >تسجيل الدخول <TbLogin2 size={25} /></Link>
         </div>
     )
 }

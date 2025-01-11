37cd23e453326aa784d95a95c3a45286e0f692fc
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-01-11 02:49:37 +0100

    `Added functionality to update user payment details in database and display user payments for the current year.`

--- app/api/year/route.ts
+++ app/api/year/route.ts
@@ -1,3 +1,4 @@
+import userPaySchema from "@/models/userPaySchema";
 import yearSchema from "@/models/yearSchema";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -5,6 +6,27 @@ export const POST = async (req: NextRequest) => {
     try {
         const { year } = await req.json()
         await yearSchema.create({ year: Number(year) })
+        await userPaySchema.updateMany({}, {
+            $push: {
+                paymentDetails: {
+                    year: Number(year),
+                    monthlyPay: [
+                        { month: "January", isPaid: false },
+                        { month: "February", isPaid: false },
+                        { month: "March", isPaid: false },
+                        { month: "April", isPaid: false },
+                        { month: "May", isPaid: false },
+                        { month: "June", isPaid: false },
+                        { month: "July", isPaid: false },
+                        { month: "August", isPaid: false },
+                        { month: "September", isPaid: false },
+                        { month: "October", isPaid: false },
+                        { month: "November", isPaid: false },
+                        { month: "December", isPaid: false }
+                    ]
+                }
+            }
+        })
         return NextResponse.json({
             message: `تمت إضافة ${year} `
         });
--- app/monthly-list/page.tsx
+++ app/monthly-list/page.tsx
@@ -104,41 +104,46 @@ const Page = () => {
                             {user?.name && user.role &&
                                 <>
                                     <button onClick={() => setAddNewYear(true)} className='p-3 bg-darkBlue text-white rounded-lg w-fit flex items-center flex-row-reverse justify-between'><TbPlus className='size-7' /> أضف سنة جديدة</button>
-                                    <button onClick={() => setAddNewUser(true)} className='p-3 bg-green-800 text-white rounded-lg w-fit flex items-center flex-row-reverse justify-between'><TbPlus className='size-7' /> أضف شخص جديدة</button>
+                                    {(years && years?.length > 0) && <button onClick={() => setAddNewUser(true)} className='p-3 bg-green-800 text-white rounded-lg w-fit flex items-center flex-row-reverse justify-between'><TbPlus className='size-7' /> أضف شخص جديدة</button>}
                                 </>
                             }
                         </div>
-                        <p className='font-semibold text-darkBlue text-2xl'>سنة {currYear}</p>
-                        <table className='table border-collapse border border-slate-500 w-full'>
-                            <thead>
-                                <tr className='table-row'>
-                                    <th className="table-cell border border-slate-600">#رقم الشقة</th>
-                                    <th className='table-cell border border-slate-600 p-5'>الاسم</th>
-                                    {months.map((month, i) =>
-                                        <th key={i} className='table-cell border border-slate-600 p-5'>{month.arabic}</th>
-                                    )}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {userPay && userPay?.length > 0 &&
-                                    userPay?.filter(user => user.role !== RoleEnum.SUPER_ADMIN).map((user, i) =>
-                                        <tr className='table-row' key={i}>
-                                            <td className='table-cell border border-slate-600 text-center'>
-                                                {user.homeNumber}
-                                            </td>
-                                            <td className='table-cell border border-slate-600 p-3 text-center'>
-                                                {user.name}
-                                            </td>
-                                            {user.paymentDetails.find(({ year }) => year == currYear)?.monthlyPay.map(({ isPaid }, i) =>
-                                                <td className='table-cell border border-slate-600 p-3' key={i}>
-                                                    {isPaid ? <IoCheckmarkCircle className='fill-green-600 size-7 w-full' /> : <IoCloseCircle className='fill-red-600 size-7 w-full' />}
-                                                </td>
-                                            )
-                                            }
+                        {
+                            ((userPay && userPay?.length > 0) || (years && years?.length > 0)) &&
+                            <>
+                                <p className='font-semibold text-darkBlue text-2xl'>سنة {currYear}</p>
+                                <table className='table border-collapse border border-slate-500 w-full'>
+                                    <thead>
+                                        <tr className='table-row'>
+                                            <th className="table-cell border border-slate-600">#رقم الشقة</th>
+                                            <th className='table-cell border border-slate-600 p-5'>الاسم</th>
+                                            {months.map((month, i) =>
+                                                <th key={i} className='table-cell border border-slate-600 p-5'>{month.arabic}</th>
+                                            )}
                                         </tr>
-                                    )}
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody>
+                                        {userPay && userPay?.length > 0 &&
+                                            userPay?.filter(user => user.role !== RoleEnum.SUPER_ADMIN).map((user, i) =>
+                                                <tr className='table-row' key={i}>
+                                                    <td className='table-cell border border-slate-600 text-center'>
+                                                        {user.homeNumber}
+                                                    </td>
+                                                    <td className='table-cell border border-slate-600 p-3 text-center'>
+                                                        {user.name}
+                                                    </td>
+                                                    {user.paymentDetails.find(({ year }) => year == currYear)?.monthlyPay.map(({ isPaid }, i) =>
+                                                        <td className='table-cell border border-slate-600 p-3' key={i}>
+                                                            {isPaid ? <IoCheckmarkCircle className='fill-green-600 size-7 w-full' /> : <IoCloseCircle className='fill-red-600 size-7 w-full' />}
+                                                        </td>
+                                                    )
+                                                    }
+                                                </tr>
+                                            )}
+                                    </tbody>
+                                </table>
+                            </>
+                        }
                     </div>
                     :
                     <div className='flex justify-center items-center h-3/4 z-0'>
